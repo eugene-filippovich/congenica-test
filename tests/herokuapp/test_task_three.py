3ca459e7e93ee859f1dc3813c9bfe32aaa6a688b
eugene-filippovich <eugene.filippovich@firedome.io>
2022-08-24 15:43:52 +0300

    Changed sleeps

--- tests/herokuapp/test_task_three.py
+++ tests/herokuapp/test_task_three.py
@@ -17,33 +17,27 @@ class TestTaskThree:
     def test_highlight_elements(self):
         header_index = challenging_dom_elemets.get_header_index(ChallengingDomElements.table_rows, "Diceret")
         challenging_dom_elemets.highlight_inner_element(ChallengingDomElements.table_rows, 3, header_index)
-        time.sleep(1)
+        time.sleep(2)
         challenging_dom_elemets.unhighlight_inner_element(ChallengingDomElements.table_rows, 3, header_index)
-        time.sleep(1)
 
         raw_with_delete = challenging_dom_elemets.find_selected_row(ChallengingDomElements.table_rows, "Apeirian7")
         challenging_dom_elemets.highlight_text(raw_with_delete, "delete")
-        time.sleep(1)
+        time.sleep(2)
         challenging_dom_elemets.unhighlight_text(raw_with_delete, "delete")
-        time.sleep(1)
 
         raw_with_edit = challenging_dom_elemets.find_selected_row(ChallengingDomElements.table_rows, "Apeirian2")
         challenging_dom_elemets.highlight_text(raw_with_edit, "edit")
-        time.sleep(1)
+        time.sleep(2)
         challenging_dom_elemets.unhighlight_text(raw_with_edit, "edit")
-        time.sleep(1)
 
         definiebas7_raw = challenging_dom_elemets.find_selected_row(ChallengingDomElements.table_rows, self.definiebas7)
         challenging_dom_elemets.highlight_text(definiebas7_raw, self.definiebas7)
-        time.sleep(1)
+        time.sleep(2)
         challenging_dom_elemets.unhighlight_text(definiebas7_raw, self.definiebas7)
-        time.sleep(1)
 
         iuvaret7_raw = challenging_dom_elemets.find_selected_row(ChallengingDomElements.table_rows, self.iuvaret7)
         challenging_dom_elemets.highlight_text(iuvaret7_raw, self.iuvaret7)
-        time.sleep(1)
+        time.sleep(2)
         challenging_dom_elemets.unhighlight_text(iuvaret7_raw, self.iuvaret7)
-        time.sleep(1)
 
         challenging_dom_elemets.click_button(ChallengingDomElements.qux_button)
-        time.sleep(1)
